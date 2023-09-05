db7b8cdae16c8d6de9f290d51e21459830fa22bf
Meiloaica Marius <meiloaica.marius@gmail.com>
2023-09-05 17:38:07 +0300

    deleted some files

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,10 +1,6 @@
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-Import-Module posh-git
-$omp_config = Join-Path $PSScriptRoot ".\morningdrunk.omp.json"
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
-
 Import-Module -Name Terminal-Icons
 
 # PSReadLine
